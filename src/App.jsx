a1fff122b7f65292057b2187dab61dc26ab5db50
presssunny <presssunny@gmail.com>
2026-04-01 16:23:48 +0300

    Add emailjs-com dependency and update App component for email input handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 
 function App() {
   const [showMessage, setShowMessage] = useState(false);
+  const [insertName, setInsertName] = useState("");
+  const [insertEmail, setInsertEmail] = useState("");
+  const [insertMessage, setInsertMessage] = useState("");
+
+  const sendEmail = () => {};
+
   return (
     <div className="card">
       <h1>Sunny Press</h1>
@@ -13,12 +19,26 @@ function App() {
       </button>
       {showMessage && (
         <div className="modal">
-          <input placeholder="insert your name "></input>
-          <input placeholder="insert your email "></input>
-          <input placeholder="message "></input>
+          <input
+            value={insertName}
+            onChange={(e) => setInsertName(e.target.value)}
+            placeholder="insert your name "
+          />
+          <input
+            value={insertEmail}
+            onChange={(e) => setInsertEmail(e.target.value)}
+            placeholder="insert your email "
+          />
+          <input
+            value={insertMessage}
+            onChange={(e) => setInsertMessage(e.target.value)}
+            placeholder="message "
+          />
           <button className="send"> send the message</button>
           <div> </div>
-          <button className=" close the window"> close the window </button>
+          <button className=" close-btn" onClick={() => setShowMessage(false)}>
+            close the window{" "}
+          </button>
         </div>
       )}
     </div>
